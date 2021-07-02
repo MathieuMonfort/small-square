78bc51e24f3f67d018de011cf8f93ba403913776
MathieuMonfort <mathieumonfort@outlook.com>
2021-07-02 17:14:22 +0200

    Even more documentation

--- source/core/input.h
+++ source/core/input.h
@@ -52,10 +52,36 @@ namespace  smallsquare {
 
     public:
 
+        /**
+         * Init should be called once in Game class constructor. it is used to assign a Window to get input information
+         * from.
+         * @param win The active window the Input class gets input information from.
+         */
+        static void Init(GLFWwindow * win);
 
-        static void Init(GLFWwindow * win );
+        /**
+         * DO NOT USE Called every time the mouse moves. It updates _mXOffset and _mYOffset so any object class can retrieve mouse
+         * input information.
+         * @param win The active window
+         * @param xpos The current mouse x position in pixel
+         * @param ypos The current mouse y position in pixel
+         */
         static void MouseMoveCallback(GLFWwindow * win, double xpos, double ypos);
+
+        /**
+         * DO NOT USE Called every time the window is resized. It updates the _winH and _winW so the viewports can
+         * update their projection matrices.
+         * @param win The active window.
+         * @param h The current window height.
+         * @param w The current window width.
+         */
         static void WindowSizeCallback(GLFWwindow * win , int h, int w);
+
+        /**
+         * Check if a key
+         * @param name
+         * @return
+         */
         static bool KeyPressed(const string &name);
         static vec2 MouseOffset();
         static vec2 MousePosition();
